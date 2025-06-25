2402c0356ae4272ecc72561e953877f7d5ba805e
zeyaulhasan <Zeyaulhasan1326@gmail.com>
2025-06-25 10:18:04 +0530

    first code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+    <h1>My First React App</h1>
+    <h2>kuchh bhi likh do </h2>
+    <h3>Welcome to my first React app</h3>
+    </div>
+  )
+}
+
+export default App
